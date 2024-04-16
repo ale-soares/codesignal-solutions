42b77d26b79ade8cffbb42006a6731938f61e53b
Alexandra Soares <alexandramesquitaa@outlook.com>
2024-04-16 15:31:06 -0300

    add first non repeating char

---
+++ interviewPractice/arrays/firstNonRepeatingCharacter.py
@@ -0,0 +1,28 @@
+# https://app.codesignal.com/interview-practice/question/uX5iLwhc6L5ckSyNC/description
+
+# Given a string s consisting of small English letters, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+
+# Example
+
+# For s = "abacabad", the output should be
+# solution(s) = 'c'.
+# There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+# For s = "abacabaabacaba", the output should be
+# solution(s) = '_'.
+# There are no characters in this string that do not repeat.
+
+def solution(s):
+  n = len(s)
+  char_count = {}
+  
+  for i in range(n):
+    if s[i] not in char_count:
+      char_count[s[i]] = 1
+    else:
+      char_count[s[i]] += 1
+          
+  for i in range(n):
+    if char_count[s[i]] == 1: return s[i]
+  
+  return '_'
